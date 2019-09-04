fb4f9c718ff7a341202eaf9d1be48d2d27d28beb
Adrian Teh <adrian@evercondo.com>
2019-09-03 20:59:09 -0400

    Multi environment docker setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Whitelist a test domain or config.hosts.clear to whitelist anything
+  config.hosts << "helloworld.test"
 end
